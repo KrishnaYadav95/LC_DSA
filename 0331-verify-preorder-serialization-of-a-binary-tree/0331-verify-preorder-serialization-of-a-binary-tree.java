360fd387f0683273a90b736ad11d596e70a33f69
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-07 23:41:35 +0530

    Time: 9 ms (9.91%), Space: 45.3 MB (6.71%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,32 +1,17 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
-         ArrayList<String> list= new ArrayList<>(Arrays.asList(preorder.split(",")));
-        int i=0;
-        int j=1;
-        // 9 3 4 # # 1 # # 2 # 6 # # 
-        // i j
-        //   i j
-        //     i j
-        //       i j
-        if(list.size() > 1 && list.get(0).equals("#")) return false;
-        while(j<list.size()){
-       if( i>0 && list.get(i).equals("#") && list.get(j).equals("#")){
-        
-        list.remove(j);
-        list.remove(i);
-        list.remove(i-1);
-      
-        list.add(i-1, "#");
-        if(list.size() > 1 && list.get(0).equals("#")) return false;
-        i=Math.max(0 ,i-2);
-        j=i+1;
-       }else{
-        i++;
-        j++;
-       }
+        if(preorder== null) return false;
+        Stack<String> st= new Stack();
+        String[] arr= preorder.split(",");
+        for(int i=0;i<arr.length;i++){
+            while(arr[i].equals("#") && !st.isEmpty() && st.peek().equals("#")){
+                st.pop();
+                if(st.isEmpty()) return false;
+                st.pop();
+            }
+            st.push(arr[i]);
         }
-        if(list.size()==1 && list.get(0).equals("#")) return true;
-        return false;
+        return st.size()==1 && st.peek().equals("#");
     }
 }
 
