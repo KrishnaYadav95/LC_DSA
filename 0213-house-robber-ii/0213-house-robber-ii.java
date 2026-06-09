57ba012665dc3b3065eebfe84b3139af68c56c6e
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-09 19:26:27 +0530

    Time: 0 ms (100%), Space: 42.6 MB (76.41%) - LeetHub - Time: O(?), Space: O(?)

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,31 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
         int[] dp1= new int[nums.length+1];
         int[] dp2= new int[nums.length+1];
         Arrays.fill(dp1, -1);
         Arrays.fill(dp2 , -1);
-        if(nums.length == 1) return nums[0];
-        return Math.max(func1(nums, 0, nums.length-2, dp1), func2(nums, 1, nums.length-1, dp2));
+        return Math.max(helper(nums ,0 , dp1 , nums.length-2) , helper(nums, 1, dp2 , nums.length-1));
     }
-
-    int func1(int[] nums, int i, int end , int[] dp1) {
-        if(i == end) return nums[i];
-        if(i > end) return 0;
-        if(dp1[i]!=-1) return dp1[i];
-        int pick = func1(nums, i+2, end , dp1) + nums[i];
-        int skip = func1(nums, i+1, end , dp1);
-        dp1[i]= Math.max(pick, skip);
-        return dp1[i];
-    }
-
-    int func2(int[] nums, int i, int end ,int[] dp2) {
-        if(i == end) return nums[i];
-        if(i > end) return 0;
-          if(dp2[i]!=-1) return dp2[i];
-        int pick = func2(nums, i+2, end , dp2) + nums[i];
-        int skip = func2(nums, i+1, end , dp2);
-        dp2[i]= Math.max(pick, skip);
-        return dp2[i];
+    int helper(int[] nums , int i  , int[] dp , int end){
+       //  if(nums.length==1) return nums[0];
+        if(i==end) return nums[end];
+        if(i>end) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int pick= nums[i]+ helper(nums , i+2, dp , end);
+        int skip= 0+ helper(nums , i+1 , dp , end);
+        dp[i]= Math.max(pick , skip);
+        return dp[i];
     }
 }
 
