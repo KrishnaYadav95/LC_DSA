62baea69ac7cc7996c0c54babf9bccbe5da0d85b
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-09 14:43:34 +0530

    Time: 0 ms (100%), Space: 42.4 MB (96.57%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length+1];
+        Arrays.fill(dp , -1);
+        return helper(nums , 0 , dp);
+    }
+    int helper(int[] nums , int i , int[] dp){
+        if(i>=nums.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int pick= helper(nums , i+2 , dp ) + nums[i];
+        int skip= helper(nums , i+1 , dp );
+         dp[i] =Math.max(pick , skip);
+         return dp[i];
+    }
+} 
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
