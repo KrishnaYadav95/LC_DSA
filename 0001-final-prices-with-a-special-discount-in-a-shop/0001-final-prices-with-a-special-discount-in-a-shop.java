f79138cc04811284185858b7754b3bfc20038ee0
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-11 12:54:59 +0530

    Time: 1 ms (99.7%), Space: 45.2 MB (74.54%) - LeetHub - Time: O(n^2), Space: O(1)

--- 0001-final-prices-with-a-special-discount-in-a-shop/0001-final-prices-with-a-special-discount-in-a-shop.java
+++ 0001-final-prices-with-a-special-discount-in-a-shop/0001-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        int[] ans= new int[prices.length];
+        //int[] ans= new int[prices.length];
         for(int i=0; i<prices.length ;i++){
-            for(int j=0;j<prices.length;j++){
-                if( j>i && prices[j]<=prices[i]){
-                    ans[i]=prices[i]-prices[j];
+            for(int j=i+1;j<prices.length;j++){
+                if(  prices[j]<=prices[i]){
+                    prices[i]=prices[i]-prices[j];
                     break;
-                }else{
-                    ans[i]=prices[i];
                 }
             }
         }
-        return ans;
+        return prices;
     }
 }
 
