4104ccc0ed95dfc5abd889ae27b465c8e43bcae9
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-13 23:38:00 +0530

    Time: 4 ms (94.26%), Space: 77.3 MB (67.57%) - LeetHub - Time: O(n), Space: O(1)

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,21 @@
 class Solution {
     public int maxArea(int[] height) {
-     int i=0;
-        int j=height.length-1;
-        int k=0;
-        int max=0;
-        while(i<j){
-  if(height[i]>height[j]) k=height[j];
-            else k= height[i];
-            max= Math.max(max, (j-i)*k);
-           if(height[i]>height[j])j--;
-            else i++;
+       int i=0;
+       int j=height.length-1;
+       int max=0;
+       int k=0;
+       while(i<j){
+        if(height[i]>height[j]){
+         k= height[j];
         }
-        return max;
+        else{ 
+        k=height[i];
+        }
+        max= Math.max(max , (j-i)*k);
+        if(height[i]>height[j]) j--;
+        else i++;
+       }
+       return max;
     }
 }
 
