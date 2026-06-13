26530a7b7f7fca5edbb238abe38ab702f7effd06
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-14 01:20:17 +0530

    Time: 0 ms (100%), Space: 46.1 MB (80.94%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0075-sort-colors/0075-sort-colors.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public void SortColors(int[] nums) {
+         int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.Length;i++){
+            if(nums[i]==0) count0++;
+            else if(nums[i]==1)  count1++;
+            else count2++;
+        }
+        int idx=0;
+        while(count0--!=0){
+            nums[idx++]=0;
+        }
+        while(count1--!=0){
+            nums[idx++]=1;
+        }
+        while(count2--!=0){
+            nums[idx++]=2;
+        }
+    
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
