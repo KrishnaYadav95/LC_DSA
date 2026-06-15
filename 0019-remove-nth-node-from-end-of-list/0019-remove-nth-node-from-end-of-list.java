1ac669900080b056087fab71e36baf12a080d7bc
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-15 14:55:34 +0530

    Time: 0 ms (100%), Space: 43.4 MB (75.76%) - LeetHub - Time: O(L), Space: O(1)

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,31 +10,25 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-       ListNode temp = head;
-       ListNode temp2 = head;
-       int count=0;
-       
-       while(temp!=null){
-        temp=temp.next;
-        count++;
-       }
-       if(count==1){
-        head=null;
+        if(head==null) return null;
+        if(head.next==null) return null;
+        int lengthofll=0;
+        ListNode temp= head;
+        while(temp!=null){
+            lengthofll+=1;
+            temp=temp.next;
+        }
+        if(n==lengthofll) return head.next;
+        int k = lengthofll-n;
+        temp= head;
+        while(k--!=1){
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
         return head;
-       }else if(count==n){
-        head=head.next;
-        return head;
-       }
-       int idx= count-n;
-       for(int i=0;i<idx-1;i++){
-        temp2=temp2.next;
-      
-       }
-       temp2.next=temp2.next.next;
-      return head;
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
