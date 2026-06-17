81ea70b22b6156675bccaf3fba42c2134f9db37e
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-17 13:52:58 +0530

    Time: 1 ms (30.26%), Space: 46.3 MB (74.33%) - LeetHub - Time: O(n), Space: O(1)

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,51 +10,50 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        // step1 count length of ll;
-        ListNode temp= head;
+        // step 1 count the length of ll;
+        ListNode temp = head;
         int count=0;
         while(temp!=null){
             count++;
             temp=temp.next;
         }
-        // reverse first k length of linkedlist
-            ListNode newHead = head;       // will become final answer
-           ListNode prevTail = null; 
+        ListNode newHead=null;
+        ListNode prevTail= null;
         while(count>=k){
-        temp=head;
-        ListNode temp2= head;
-        ListNode temp3=head;
-   
-        for(int i=0;i<k-1;i++){
-            temp2=temp2.next;
+            temp=head;
+            ListNode temp2=head;
+            ListNode temp3= head;
+            for(int i=0;i<k-1;i++){
+                temp2=temp2.next;
+            }
+            temp3=temp2.next;
+            ListNode newnode= reverse(temp , temp2);
+            if(prevTail==null) newHead=newnode;
+            else {
+                prevTail.next=newnode;
+            }
+            // temp is now tail of reversed group
+            temp.next=temp3;
+            prevTail=temp;
+            head=temp3;
+            count-=k;
         }
-        temp3=temp2.next;
-    ListNode newnode=  reverse(temp , temp2);
-    if(prevTail==null) newHead= newnode;
-    else prevTail.next=newnode;
-    // temp is now tail of reversed group 
-    temp.next=temp3;
-    prevTail=temp;
-    head=temp3;
-    count=count-k;
-
-        }
-return newHead;
+        return newHead;
     }
-   ListNode reverse(ListNode head , ListNode tail){
-     ListNode stop = tail.next;
+    ListNode reverse(ListNode head , ListNode tail){
+        ListNode stop= tail.next;
+        // why tail.next becoz we also have to reverse the tail node so we have to include that too
         ListNode curr= head;
         ListNode next= null;
         ListNode prev= null;
-        while(curr != stop){
+        while(curr!=stop){
             next=curr.next;
             curr.next=prev;
             prev=curr;
-            curr= next;
+            curr=next;
+        }
+        return prev;
     }
-    return prev;
-    }
-
 }
 
 // Synced seamlessly with LeetHub Pro
