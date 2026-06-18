7fd4490ae11f5442fe3122b6818d5b88d3c2049a
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-18 10:08:37 +0530

    Added solution - LeetHub

---
+++ 706123--GFG/706123-.java
@@ -0,0 +1,27 @@
+class Solution {
+    static Boolean isSubsetSum(int arr[], int sum) {
+        int n = arr.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        
+        // Base case: sum = 0 is always achievable
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                dp[i][j] = dp[i - 1][j]; // skip current element
+                if (j >= arr[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]]; // pick current element
+                }
+            }
+        }
+        
+        return dp[n][sum];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
