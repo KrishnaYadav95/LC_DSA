33b5679c3c813d8c8d11cb9d04c75cf47dc3d2ef
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-19 10:08:21 +0530

    Time: 0 ms (100%), Space: 42.9 MB (87.36%) - LeetHub - Time: O(n), Space: O(1)

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,15 +1,26 @@
+// class Solution {
+//     public int largestAltitude(int[] gain) {
+//         ArrayList<Integer> list= new ArrayList<>();
+//         list.add(0 , 0);
+//         list.add(1, gain[0]);
+//         int sum=gain[0];
+//         for(int i=1;i<gain.length;i++){
+//             sum+=gain[i];
+//             list.add(sum);
+//         }
+//         Collections.sort(list);
+//         return list.get(list.size()-1);
+//     }
+// }
 class Solution {
     public int largestAltitude(int[] gain) {
-        ArrayList<Integer> list= new ArrayList<>();
-        list.add(0 , 0);
-        list.add(1, gain[0]);
-        int sum=gain[0];
-        for(int i=1;i<gain.length;i++){
-            sum+=gain[i];
-            list.add(sum);
+        int sum = 0;
+        int maxAltitude = 0;
+        for(int i = 0; i < gain.length; i++){
+            sum += gain[i];
+            maxAltitude = Math.max(maxAltitude, sum);
         }
-        Collections.sort(list);
-        return list.get(list.size()-1);
+        return maxAltitude;
     }
 }
 
