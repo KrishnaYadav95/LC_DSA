90fdb1505352141c5e3b759492b44a4c590fb46d
Krishna Yadav <krishnayadav0816@gmail.com>
2026-06-23 17:41:44 +0530

    Time: 1 ms (97.83%), Space: 42.6 MB (60.94%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+        int low=1;
+        int high=x;
+        int ans=1;
+        while(low<=high){
+            int mid =low+(high-low)/2;
+            if((long)mid*mid==x) return mid;
+            else if ((long)mid*mid >x){
+                high=mid-1;
+            }else{
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
