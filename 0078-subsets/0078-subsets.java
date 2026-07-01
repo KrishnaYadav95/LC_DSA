26c181e2c35ade9eac38ad96ce68d4385e964fda
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-01 18:39:35 +0530

    Time: 1 ms (88.18%), Space: 44.4 MB (20.41%) - LeetHub - Time: O(2^n), Space: O(n)

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        List<Integer> list= new ArrayList<>();
+        subset(nums, 0 , list, ans);
+        return ans;
+    }
+    void subset(int[] nums , int idx , List<Integer> list , List<List<Integer>> ans){
+
+        if(idx>=nums.length){
+            ans.add(new ArrayList<>(list));
+            return ;
+        }
+        //pick
+        list.add(nums[idx]);
+        subset(nums , idx+1 , list , ans);
+        list.remove(list.size()-1);
+
+        // dont pick
+        subset(nums , idx+1 , list, ans);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
