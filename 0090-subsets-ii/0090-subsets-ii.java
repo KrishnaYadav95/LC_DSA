d9005226c3cfabf7c90ceb9c88b90f4a6c801fdf
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-02 18:34:23 +0530

    Time: 2 ms (99.71%), Space: 45.2 MB (42.66%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+     List<List<Integer>> ans= new ArrayList<>();
+     List<Integer> list= new ArrayList<>();
+     subset(nums, 0 , list , ans);
+     return ans;   
+    }
+    void subset(int[] nums , int idx , List<Integer> list , List<List<Integer>> ans ){
+
+        if(idx==nums.length){
+            ans.add(new ArrayList<>(list));
+            return ;
+        }
+
+        if(idx>=nums.length){
+            return ;
+        }
+        // pick 
+        list.add(nums[idx]);
+        subset(nums, idx+1 , list, ans );
+        list.remove(list.size()-1);
+        
+        while(idx<nums.length-1 && nums[idx]==nums[idx+1]) idx++;
+
+        //dont pick
+        subset(nums , idx+1 , list, ans );
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
