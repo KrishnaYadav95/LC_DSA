876cbc1c6375c0356d08f657cb3fb5c9104a3e92
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-02 20:00:43 +0530

    Time: 1 ms (87.28%), Space: 45.3 MB (90.14%) - LeetHub - Time: O(n*n!), Space: O(n)

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,26 +3,22 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         boolean[] flag = new boolean[nums.length];
-        perm(nums, 0, list, ans ,flag);
+        perm(nums,  list, ans ,flag);
         return ans;
     }
 
-    void perm(int[] nums, int idx, List<Integer> list, List<List<Integer>> ans , boolean[] flag) {
+    void perm(int[] nums,  List<Integer> list, List<List<Integer>> ans , boolean[] flag) {
 
         if (list.size() == nums.length) {
             ans.add(new ArrayList<>(list));
             return;
         }
-        if (idx >= nums.length) {
-            return;
-        }
-
         //pick
         for (int i = 0; i < nums.length; i++) {
             if (!flag[i]) {
                  flag[i] = true;
                 list.add(nums[i]);
-                perm(nums, idx+1  ,list, ans , flag);
+                perm(nums ,list, ans , flag);
                 list.remove(list.size() - 1);
                 flag[i] = false;
             }
