fac81bbaf9f1ff9bd55c5c1d2c5e5ac5585e3f31
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-03 12:15:45 +0530

    Time: 1 ms (99.46%), Space: 42.9 MB (52.29%) - LeetHub - Time: O(n log n), Space: O(n)

--- 0001-last-stone-weight/0001-last-stone-weight.java
+++ 0001-last-stone-weight/0001-last-stone-weight.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int lastStoneWeight(int[] arr) {
-     PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-     for( int i=0; i<arr.length;i++){
-        pq.add(arr[i]);
-        }
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer>  pq= new PriorityQueue<>(Collections.reverseOrder());
+        for(int i: stones) pq.add(i);
+
         while(pq.size()>1){
-            int x = pq.remove();
-            int y = pq.remove();
-            int z = x - y;
+            int x= pq.remove();
+            int y= pq.remove();
+            int z= x-y;
             if(z>0) pq.add(z);
         }
-        if(pq.size() == 1) return pq.peek();
-        else return 0;
+        return pq.size()==0? 0 : pq.peek();
     }
 }
 
