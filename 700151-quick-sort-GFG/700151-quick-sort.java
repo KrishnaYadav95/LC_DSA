94122e8d016c27c9f145d481d8b781d2f6472df5
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-03 16:31:23 +0530

    Added solution - LeetHub

---
+++ 700151-quick-sort-GFG/700151-quick-sort.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void quickSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        int pIndex = partition(arr, low, high);
+        quickSort(arr, low, pIndex - 1);
+        quickSort(arr, pIndex + 1, high);
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high - 1 && arr[i] <= pivot) {
+                i++;
+            }
+
+            while (j >= low + 1 && arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
