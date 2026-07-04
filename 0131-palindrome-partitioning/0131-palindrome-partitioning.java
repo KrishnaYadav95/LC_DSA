b404fda67d284545586aa26b22e41d06fa26de1f
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-04 18:09:28 +0530

    Time: 8 ms (74.4%), Space: 65 MB (87.22%) - LeetHub - Time: O(2^n * n), Space: O(n)

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,38 +1,33 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ans= new ArrayList<>();
+         List<List<String>> ans= new ArrayList<>();
         List<String> list = new ArrayList<>();
-        part(s, 0 , list, ans);
+        part(s, 0 ,  ans, list);
         return ans;
     }
-    void part(String s , int idx , List<String> list , List<List<String>> ans){
-
-
-        if(idx==s.length()){
-            ans.add(new ArrayList<>(list));
-            return ;
-        }
-        if(idx>=s.length()){
-            return ;
-        }
-
-        for(int i=idx; i<s.length();i++){
-            if(isPallindrome(s, idx , i)){
-            list.add(s.substring(idx , i+1));
-            part(s, i+1 , list, ans);
-            list.remove(list.size()-1);
-            }
-        }
-    }boolean isPallindrome(String s , int i , int j){
+    boolean isPallindrome(String s , int i , int j){
         while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
-            }else{
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            else {
                 i++;
                 j--;
             }
         }
         return true;
+    }
+    void part(String s , int idx , List<List<String>> ans , List<String> list){
+
+if(idx==s.length()){
+    ans.add(new ArrayList<>(list));
+    return ;
+}
+        for(int i=idx; i<s.length();i++){
+            if(isPallindrome(s, idx , i)){
+                list.add(s.substring(idx, i+1));
+                part(s, i+1 , ans , list);
+                list.remove(list.size()-1);
+            }
+        }
     }
 }
 
