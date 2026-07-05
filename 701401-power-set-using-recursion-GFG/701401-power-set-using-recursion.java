bfd7e076fa32af769272fb3c580d23bc7054ecb6
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-05 17:56:46 +0530

    Added solution - LeetHub

--- 701401-power-set-using-recursion-GFG/701401-power-set-using-recursion.java
+++ 701401-power-set-using-recursion-GFG/701401-power-set-using-recursion.java
@@ -1,31 +1,29 @@
 class Solution {
     public ArrayList<String> powerSet(String s) {
         // code here
-        char []arr= new char[s.length()];
-        for(int i=0;i<s.length();i++){
-            arr[i]= s.charAt(i);
-        }
-        ArrayList<String> list= new ArrayList<>();
-         ArrayList<Character> curr= new ArrayList<>();
-        set(arr, list, 0 , curr);
-        return list;
+        StringBuilder sb= new StringBuilder();
+        ArrayList<String> ans= new ArrayList<>();
+        set(s, 0 , sb , ans);
+        return ans;
+        
     }
-    void set(char[] arr , ArrayList<String> list , int i , ArrayList<Character> curr){
-       if(i>=arr.length){
-          // list.add(arr);
-          StringBuilder sb= new StringBuilder();
-          for(char ch: curr){
-              sb.append(ch);
-          }
-          list.add(sb.toString());
-           return ;
-       }
-       //take it 
-       curr.add(arr[i]);
-       set(arr , list, i+1 , curr);
-       curr.remove(curr.size()-1);
-       // not take it
-       set(arr, list , i+1 , curr);
+    void set(String s , int idx , StringBuilder sb , ArrayList<String> ans){
+        
+        if(idx==s.length()){
+            ans.add(sb.toString());
+            return ;
+        }
+        if(idx>=s.length()){
+            return ;
+        }
+        
+        // pick the element 
+        sb.append(s.charAt(idx));
+        set(s, idx+1 , sb , ans);
+        sb.deleteCharAt(sb.length()-1);
+        
+        //dont pick 
+        set(s, idx+1 , sb , ans);
     }
 }
 
