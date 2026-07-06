2684f2a5b6b5468237e9e80f237bc7858d449039
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-06 16:19:59 +0530

    Time: 8 ms (15.84%), Space: 45.5 MB (29.02%) - LeetHub - Time: O(2^n), Space: O(n)

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,42 +1,32 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
-
-    // There is one more way to avoid duplicate in this problem since flag is not working we have to sort the array first ..
-
-    // we can remove the boolean flag array since it is useless.. now
-    Arrays.sort(nums);
-
-         List<List<Integer>> ans= new ArrayList<>();
-         List<Integer> list= new ArrayList<>();
-        // boolean[] flag= new boolean[nums.length+1];
-         combination(nums , 0 ,0 ,target , list, ans);// fahhhhhhhhhhhhhhh
-         return ans;
+        // we need to sort the array first to handle duplicates
+        Arrays.sort(nums);
+        List<List<Integer>> ans= new ArrayList<>();
+        List<Integer> list= new ArrayList<>();
+        combination(nums , 0 , 0 , target, list , ans);
+        return ans;
     }
-    void combination(int[] nums , int idx , int sum,int target , List<Integer> list , List<List<Integer>> ans  ){
-
-        //Base conditions
+    void combination(int[] nums , int idx, int sum , int target , List<Integer> list , List<List<Integer>> ans){
+        // Base case
         if(sum==target){
             ans.add(new ArrayList<>(list));
-            return ;
-        }
-        if(idx>=nums.length || sum> target){
             return;
         }
-        // pick the index
-    
+        if(sum> target|| idx>=nums.length){
+            return ;
+        }
+        // case-1 pick it
         list.add(nums[idx]);
         sum+=nums[idx];
-       
-        combination(nums , idx+1 , sum , target, list, ans);
+        combination(nums, idx+1 , sum , target , list , ans);
         list.remove(list.size()-1);
         sum-=nums[idx];
-    
-    while(idx<nums.length-1 && nums[idx]==nums[idx+1]) idx++;
-// what this line does is since array is sorted now .. so when two duplicates are detected it automatically do i++ till duplicates are found.
 
-// lets submit our code..
-        // dont pick
-        combination(nums , idx+1 , sum , target, list, ans);
+        while(idx<nums.length-1 && nums[idx]==nums[idx+1]) idx++;
+
+        //case-2 Dont pick it
+        combination(nums, idx+1 , sum , target , list , ans);
     }
 }
 
