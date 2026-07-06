50bd222b15a7da4627e62dcf9a0e9595cfaee0ad
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-06 18:11:57 +0530

    Time: 0 ms (100%), Space: 42.5 MB (29.86%) - LeetHub - Time: O(?), Space: O(?)

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,29 +1,35 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> list= new ArrayList<>();
-        combination(1, n , k , list , ans, 0);
-        return ans;
+     List<Integer> list= new ArrayList<>();
+     List<List<Integer>> ans= new ArrayList<>();
+     boolean[] flag = new boolean[10];
+     combination(k , n ,1 , 0, list , ans , flag);
+     return ans;   
     }
-    void combination(int idx , int n , int k  , List<Integer> list , List<List<Integer>> ans , int sum){
+    void combination(int k , int n , int i , int sum , List<Integer> list, List<List<Integer>> ans, boolean[] flag){
+        // Base case-1
         if(sum==n && list.size()==k){
             ans.add(new ArrayList<>(list));
-            return;
-        }
-        if(idx>9){
             return ;
         }
+        if(sum>n) return ;
+        if(i>9) return ;
 
-        list.add(idx);
-        sum+=idx;
-        combination(idx+1 , n, k , list, ans , sum);
+        //step1 pick it
+      
+        list.add(i);
+        sum+=i;
+        flag[i]=true;
+        combination(k , n ,i+1,sum ,list, ans , flag);
         list.remove(list.size()-1);
-        sum-=idx;
-
-        //dont pick
-        combination(idx+1 , n , k , list, ans, sum);
+        sum-=i;
+      
+      // skip it 
+      combination(k , n , i+1 ,sum , list, ans, flag);
+        }
     }
-}
+  
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
