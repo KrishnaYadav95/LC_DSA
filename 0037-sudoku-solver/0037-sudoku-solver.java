271ef0b510187cd370bc3d8e81c68b6b31f5754b
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-12 14:38:49 +0530

    Time: 381 ms (18.23%), Space: 42.9 MB (34.64%) - LeetHub - Time: O(9^(n*n)), Space: O(n*n)

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -4,18 +4,15 @@ class Solution {
     }
     boolean solve(char[][] board){
         for(int i=0;i<board.length;i++){
-            for(int j=0; j<board[0].length ; j++){
+            for(int j=0;j<board[0].length;j++){
                 if(board[i][j]=='.'){
-                    for(char c='1';c<='9';c++){
-                        if(isValid(board , i , j, c)){
-                            board[i][j]=c;
-
-                            if(solve(board))
-                                return true;
-                            
-                             else   board[i][j]='.';
-                        }
+                    for(char c='1'; c<='9';c++){
+                    if(isValid(board, i , j , c)){
+                        board[i][j]=c;
+                        if(solve(board)) return true;
                     }
+                    }
+                    board[i][j]='.';
                     return false;
                 }
             }
@@ -23,21 +20,14 @@ class Solution {
         return true;
     }
     boolean isValid(char[][] board , int row , int col , char c){
-        for(int i=0;i<9; i++){
-            if(board[i][col]==c){
-                return false;
-            }
-            if(board[row][i]==c){
-                return false;
-            }
-            if(board[3 * (row/3)+ (i/3)][3*(col/3)+ (i%3)]==c){
-                return false;
-            }
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c) return false;
+            if(board[row][i]==c) return false;
+            if(board[3*(row/3)+ i/3][3*(col/3)+ i%3]==c) return false;
         }
         return true;
     }
 }
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
