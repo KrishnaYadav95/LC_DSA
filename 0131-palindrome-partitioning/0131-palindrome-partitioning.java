a481aaaa77c001d6cedb9d3d9cc00c5a5265055f
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-16 22:16:17 +0530

    Time: 9 ms (26.54%), Space: 65.2 MB (74.84%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,30 +1,27 @@
 class Solution {
     public List<List<String>> partition(String s) {
-         List<List<String>> ans= new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        part(s, 0 ,  ans, list);
+        List<List<String>> ans= new ArrayList<>();
+        List<String> list= new ArrayList<>();
+        parti(s, 0 , list, ans);
         return ans;
     }
-    boolean isPallindrome(String s , int i , int j){
+    boolean isPallindrome(String s  ,int i , int j){
         while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            else {
-                i++;
-                j--;
-            }
+            if(s.charAt(i++)!=s.charAt(j--))
+            return false;
         }
         return true;
     }
-    void part(String s , int idx , List<List<String>> ans , List<String> list){
-
-if(idx==s.length()){
-    ans.add(new ArrayList<>(list));
-    return ;
-}
-        for(int i=idx; i<s.length();i++){
-            if(isPallindrome(s, idx , i)){
-                list.add(s.substring(idx, i+1));
-                part(s, i+1 , ans , list);
+    void parti(String s , int idx , List<String> list , List<List<String>> ans){
+        // bas case
+        if(idx==s.length()){
+            ans.add(new ArrayList<>(list));
+            return ;
+        }
+        for(int i=idx;i<s.length();i++){
+            if(isPallindrome(s, idx , i)){// i was checking i+1 thats out of bound 
+                list.add(s.substring(idx , i+1));// i+1 is excluding string from idx to i but it is extrinsc fahh
+                parti(s, i+1 , list, ans);
                 list.remove(list.size()-1);
             }
         }
