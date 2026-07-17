9764355bd79bde73d434bed51d78b5fa3f2d16a5
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-17 22:20:57 +0530

    Time: 1 ms (99.65%), Space: 42.9 MB (60.06%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,21 +1,21 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        List<Integer> numbers= new ArrayList<>();
+        ArrayList<Integer> list= new ArrayList<>();
         int fact=1;
-        for(int i=1;i<=n;i++){
-            numbers.add(i);
-            fact*=i;
+        for(int i=1;i<=n ; i++){
+            list.add(i);
+            fact=fact*i;
         }
         k--;
-        StringBuilder result= new StringBuilder();
+        StringBuilder sb= new StringBuilder();
         for(int i=0;i<n;i++){
-            fact= fact/ (n-i);
+            fact=fact/(n-i);
             int index= k/fact;
-            result.append(numbers.get(index));
-            numbers.remove(index);
+            sb.append(list.get(index));
+            list.remove(index);
             k=k%fact;
         }
-        return result.toString();
+        return sb.toString();
     }
 }
 
