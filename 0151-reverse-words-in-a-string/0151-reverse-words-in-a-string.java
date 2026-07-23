d76c2f2339a2120f8c5ff9a7056a3badfacfe968
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-23 19:58:31 +0530

    Time: 6 ms (80.59%), Space: 45.2 MB (21.88%) - LeetHub - Time: O(n), Space: O(n)

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,13 +1,44 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words= s.split(" +");StringBuilder sb = new StringBuilder();
-        for(int i=words.length-1;i>=0;i--){
-            sb.append(words[i]);
-            sb.append(" ");
+        // step1-> reverse the full string
+        // eulb si yks eht
+        // step2-> reverse each word again
+        // blue is sky the
+        
+        
+        s=s.trim();
+        ArrayList<String> list= new ArrayList<>();
+        StringBuilder sb= new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--){
+            sb.append(s.charAt(i));
         }
-       
-        return sb.toString().trim();
+        int i=0;
+        int j=0;
+        while(j<sb.length()){
+            if(sb.charAt(j)==' '){
+                if(i!=j){
+               list.add(reverse(sb , i ,j));
+               list.add(" ");
+                }
+                i=j+1;
+            }
+            j++;
+        }
+       if(i!=j) list.add(reverse(sb , i, j)) ;// to add the last word
+        StringBuilder ans= new StringBuilder();
+        for(int l=0;l<list.size();l++){
+            ans.append(list.get(l));
+        }
+        return ans.toString();
     }
+String reverse(StringBuilder sb , int i , int j){
+    StringBuilder str= new StringBuilder();
+    for(int k=j-1 ; k>=i ; k--){
+        str.append(sb.charAt(k));
+    }
+   
+    return str.toString();
+}
 }
 
 // Synced seamlessly with LeetHub Pro
