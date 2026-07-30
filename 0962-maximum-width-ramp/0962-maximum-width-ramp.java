45b3582599293a75f8cad5be9a93d75e2c027975
Krishna Yadav <krishnayadav0816@gmail.com>
2026-07-30 22:06:29 +0530

    Time: 4 ms (91.96%), Space: 59.4 MB (86.94%) - LeetHub - Time: O(n), Space: O(n)

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.java
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
-       int[] rightmax= new int[nums.length];
-       int n=nums.length;
-       rightmax[n-1]= nums[n-1];
-       for(int i=n-2;i>=0;i--){
-        rightmax[i]= Math.max(rightmax[i+1] , nums[i]);
-       }
-       int max=0;
-       int i=0;
-       int j=0;
-       while(j<n){
-        while(i<j && nums[i]> rightmax[j]){
-            i++;
+        int[] rightmax= new int[nums.length];
+        int n=nums.length;
+        rightmax[n-1]=nums[n-1];
+        for(int k=n-2;k>=0;k--){
+            rightmax[k]=Math.max(nums[k] , rightmax[k+1]);
         }
-        max=Math.max(max, j-i);
-        j++;
-       }
-       return max;
+        int i=0;
+        int j=0;
+        int maxwidth=0;
+        while(j<nums.length){
+            while( i<j && nums[i]>rightmax[j]){
+                i++;
+            }
+      
+            maxwidth=Math.max(maxwidth ,j-i);
+                    j++;
+        }
+        return maxwidth;
     }
 }
 
