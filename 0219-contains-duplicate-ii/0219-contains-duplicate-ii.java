e80da2ad6c5b67e08722d22d4dd2241c2f40a60f
Krishna Yadav <krishnayadav0816@gmail.com>
2026-08-12 19:47:14 +0530

    Time: 30 ms (21.29%), Space: 92 MB (42.2%) - LeetHub - Time: O(n), Space: O(min(n, k))

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -2,24 +2,24 @@ class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
         int i = 0;
-        int j = 0;
-        
-        while (j < nums.length) {
+
+        for (int j = 0; j < nums.length; j++) {
+
             if (set.contains(nums[j])) {
                 return true;
             }
+
             set.add(nums[j]);
-            
+
             if (j - i >= k) {
                 set.remove(nums[i]);
                 i++;
             }
-            j++;
         }
+
         return false;
     }
 }
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
