9d3f16b8b8ab867bc5eb14b31a1140056bd20b05
Krishna Yadav <krishnayadav0816@gmail.com>
2026-09-04 12:09:14 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (32.13%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,26 +1,26 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         // find left maximum array
-        List<Integer> list1= new ArrayList<>();
+    int[] arr1= new int[nums.length];
          int max=nums[0];
         for(int i=0;i<nums.length;i++){
             max=Math.max(max, nums[i]);
-            list1.add(max);
+           arr1[i]=max;
             
         }
         // right minimum array 
-         List<Integer> list2= new ArrayList<>();
+         int[] arr2= new int[nums.length];
          int min=nums[nums.length-1];
          for(int i=nums.length-1 ; i>=0;i--){
               min=Math.min(min, nums[i]);
-            list2.add(0, min);
+            arr2[i]=min;
           
         }
         // 5 5 5 5 list1
         // 0 0 1 4 list2
         // now subtract list1-list2
-        for(int i=0;i<list1.size();i++){
-            int diff=list1.get(i)-list2.get(i);
+        for(int i=0;i<arr1.length;i++){
+            int diff=arr1[i]-arr2[i];
             if(diff<=k){
                 return i;
             }
