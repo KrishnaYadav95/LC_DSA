513b5b2cd1b79cf773f33cccb37af468f95911b3
Krishna Yadav <krishnayadav0816@gmail.com>
2026-09-14 00:00:34 +0530

    Added solution - Time: O(n*m), Space: O(n*m)

--- 703201-longest-common-substring-GFG/703201-longest-common-substring.java
+++ 703201-longest-common-substring-GFG/703201-longest-common-substring.java
@@ -1,26 +1,17 @@
 class Solution {
-    int max=0;
     public int longCommSubstr(String s1, String s2) {
-        // code here
-        int[][]dp= new int[s1.length()+1][s2.length()+1];
-        for(int [] i: dp) Arrays.fill(i, -1);
-         f(s1, s2, s1.length()-1, s2.length()-1 , dp );
-         return max;
-    }
-    int f(String s1 , String s2 , int i, int j , int[][] dp ){
-        if(i<0 || j<0) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            dp[i][j]= 1+ f(s1, s2, i-1, j-1, dp);
-            max= Math.max(dp[i][j] , max);
-        }else{
-            dp[i][j]=0;
-            
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        int max = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
         }
-        f(s1, s2, i-1, j, dp);
-            f(s1, s2, i, j-1, dp);
-         return dp[i][j];
+        return max;
     }
 }
 
