f390296b08f67242c7858251697d874b7211b52e
Krishna Yadav <krishnayadav0816@gmail.com>
2026-09-13 23:36:56 +0530

    Time: 27 ms (22.6%), Space: 53.9 MB (52.66%) - LeetHub - Time: O(N*M), Space: O(N*M)

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+        for(int [] i:dp) Arrays.fill(i, -1);
+        return f(text1 , text2 , text1.length()-1 , text2.length()-1 , dp);
+    }
+    int f(String s1 , String s2 , int i , int j , int[][] dp){
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)){
+            dp[i][j]= 1+f(s1, s2, i-1 ,j-1, dp);
+        }else{
+            dp[i][j]=  Math.max(f(s1, s2, i-1, j, dp) , f(s1 , s2, i, j-1,dp));
+        }
+        return dp[i][j];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
