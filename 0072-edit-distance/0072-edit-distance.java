84d456a863a3512d3431b113570d7fee3c238051
Krishna Yadav <krishnayadav0816@gmail.com>
2026-09-15 22:49:24 +0530

    Time: 3 ms (98.06%), Space: 47.1 MB (70.67%) - LeetHub - Time: O(N*M), Space: O(N*M)

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,17 +2,17 @@ class Solution {
     public int minDistance(String s1, String s2) {
         int[][] dp = new int[s1.length()+1][s2.length()+1];
         for(int [] i: dp) Arrays.fill(i, -1);
-        return f(s1, s2, 0 , 0 , dp);
+        return f(s1, s2, s1.length()-1 , s2.length()-1 , dp);
     }
     int f(String s1, String s2, int i, int j , int[][] dp){
-        if(i==s1.length()) return s2.length()-j;
-        if(j==s2.length()) return s1.length()-i;
+        if(i<0) return j+1;
+        if(j<0) return i+1;
         
         if(dp[i][j]!=-1) return dp[i][j];
         if(s1.charAt(i)==s2.charAt(j)){
-            dp[i][j]= f(s1, s2, i+1 , j+1 , dp);
+            dp[i][j]= f(s1, s2, i-1 , j-1 , dp);
         }else{
-            dp[i][j]=1+ Math.min(f(s1, s2, i+1 , j, dp) , Math.min( f(s1 , s2, i, j+1 , dp) , f(s1,s2, i+1, j+1 , dp) ));
+            dp[i][j]=1+ Math.min(f(s1, s2, i-1 , j, dp) , Math.min( f(s1 , s2, i, j-1 , dp) , f(s1,s2, i-1, j-1 , dp) ));
         }
         return dp[i][j];
     }
