5b453f6fc778c493e4e84c75c734adf3ff6e3bc1
Krishna Yadav <krishnayadav0816@gmail.com>
2026-09-23 06:50:36 +0530

    Time: 4 ms (98.07%), Space: 102 MB (76.11%) - LeetHub - Time: O(n), Space: O(1)

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,8 +1,15 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
+
        int total =0;
        for(int i:nums) total +=i;
+       
        int target= total -x;
+       // why we are finding total -x 
+
+       // because we sre taking element from both ends , total-x gives element to keep that is middle elements .. 
+       // so we will find longest length of element to keep so that minimum length is left behind
+       
        if(target<0) return -1;
         int i=0;
         int j=0;
@@ -18,6 +25,9 @@ class Solution {
             if(sum==target ){
                 ans= Math.max(ans , j-i+1);
             }
+            // we are finding maxlength of element to keep.... so it automatically gives least  element to remove .. 
+
+            //then least element to keep will be nums.length- ans..
             j++;
         }
         return ans==-1 ? -1: nums.length- ans;
